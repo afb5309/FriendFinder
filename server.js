cc6c0276ea12eab6b337d0c9db67d7649585ecd0
Alex <afb5309@psu.edu>
2019-05-06 23:10:46 -0400

    lala

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "app/public/home.html"));
+});
